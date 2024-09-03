d29386f5aca2c90ad48eae3826de70b5b8501052
AxoltDash <darshan@ciencias.unam.mx>
2024-09-02 23:00:45 -0600

    first commit

---
+++ src/main/java/net/axoltdash/obelisks/ObelisksClient.java
@@ -0,0 +1,10 @@
+package net.axoltdash.obelisks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ObelisksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Hello Fabric Client world!");
+    }  
+}
---
+++ src/main/java/net/axoltdash/obelisks/ObelisksDataGenerator.java
@@ -0,0 +1,11 @@
+package net.axoltdash.obelisks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ObelisksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
