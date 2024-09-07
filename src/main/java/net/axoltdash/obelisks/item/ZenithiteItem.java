9447c92e14fe96798bb601b34db1b5d22e286580
AxoltDash <darshan@ciencias.unam.mx>
2024-09-07 02:28:59 -0600

    ✨ Add Crafting Zenithite, Add sound Teleporting from Zenithite, Nerf range of teleport frmo Zenithite

--- src/main/java/net/axoltdash/obelisks/item/ZenithiteItem.java
+++ src/main/java/net/axoltdash/obelisks/item/ZenithiteItem.java
@@ -3,7 +3,8 @@ package net.axoltdash.obelisks.item;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsageContext;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -22,13 +23,17 @@ public class ZenithiteItem extends Item {
         ItemStack itemStack = player.getStackInHand(hand);
 
         if (!world.isClient) {
-            HitResult hitResult = player.raycast(200, 0.0F, false);
+            HitResult hitResult = player.raycast(32, 0.0F, false);
             if (hitResult.getType() == HitResult.Type.BLOCK) {
                 BlockHitResult blockHitResult = (BlockHitResult) hitResult;
                 Vec3d targetPos = blockHitResult.getPos();
 
                 player.requestTeleport(targetPos.x, targetPos.y, targetPos.z);
                 itemStack.decrement(1); // Remove one item from the stack
+
+                // Play Enderman teleport sound
+                world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
+
                 return new TypedActionResult<>(ActionResult.SUCCESS, itemStack);
             }
         }
